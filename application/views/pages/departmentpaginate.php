b78faf013f167ab3c7f96733e109d21fff5e731d
Mark Kevin Besinga <besingamkb@github.com>
2014-09-11 10:17:30 +0800

    september 11 2014

--- application/views/pages/departmentpaginate.php
+++ application/views/pages/departmentpaginate.php
@@ -19,6 +19,7 @@
                     <table class="table table-striped table-bordered table-hover dataTable no-footer" id="dataTables-example" aria-describedby="dataTables-example_info">
                         <thead>
                             <tr role="row">
+                                <th class="" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column ascending" style="width: 50px;"></th>
                                 <th class="" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column ascending" style="width: 173px;">Lastname:</th>
                                 <th class="" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 222px;">Firstname: </th>
                                 <th class="" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 203px;">Department: </th>
@@ -30,7 +31,8 @@
                             <!-- rkey, empno, surname, firstname, middlename, address, birthday, age, gender, religion, position, deptid, division, datehire, gsis, philhealth, tin, ioe, email -->
                             <?php foreach($result as $row): ?>
                             <tr class="gradeA odd">
-                                <td class="sorting_1"><a href="<?php echo base_url(); ?>index.php/user/profile/<?php echo $row->empno ?>"><?php echo $row->surname ?></a></td>
+                                <td width="50"><a href="<?php echo base_url(); ?>index.php/user/profile/<?php echo $row->empno ?>"><i class="fa fa-edit"></i></a> <a href="" id="delete"><i class="fa fa-times"></a></i></td>
+                                <td class="sorting_1"><?php echo $row->surname ?></td>
                                 <td class=" "><?php echo $row->firstname ?></td>
                                 <td class=" "><?php echo $row->deptname ?></td>
                                 <td class="center "><?php echo $row->email ?></td>
